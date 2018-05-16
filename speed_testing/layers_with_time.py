b94a1b5bfb021e2d0e924db55ca9629f03136a53
lhq1208 <1972573764@qq.com>
2018-05-16 14:25:22 +0800

    update

--- speed_testing/layers_with_time.py
+++ speed_testing/layers_with_time.py
@@ -1,109 +1,112 @@
+"""
+Add attribute times in each module
+"""
+
+
 from torch import nn
-from speed_testing.cul_speed import CulTime
 import time
 import torch
 from torch.autograd import Variable
+from torch.nn import functional as F
 
 
-class ConvWithTime(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, layer_name, stride=1,
+class Conv2dWithTime(nn.Conv2d):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1,
                  padding=0, dilation=1, groups=1, bias=True):
-        super(ConvWithTime, self).__init__()
+        super(Conv2dWithTime, self).__init__(in_channels, out_channels, kernel_size,
+                                             stride, padding, dilation, groups, bias)
+        self.name = 'conv2d'
         self.times = 0
-        self.layer_name = layer_name
-        self.conv = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size,
-                              stride=stride, padding=padding, bias=bias, groups=groups, dilation=dilation
-                              )
 
-    def forward(self, x):
+    def forward(self, input):
         tic = time.time()
-        x = self.conv(x)
+        output = F.conv2d(input, self.weight, self.bias, self.stride,
+                          self.padding, self.dilation, self.groups)
         toc = time.time()
         self.times = toc-tic
-        return x
+        return output
 
 
-class BNWithTime(nn.Module):
-    def __init__(self, num_features, layer_name):
-        super(BNWithTime, self).__init__()
+class BatchNorm2dWithTime(nn.BatchNorm2d):
+    def __init__(self, num_features):
+        super(BatchNorm2dWithTime, self).__init__(num_features=num_features)
         self.times = 0
-        self.layer_name = layer_name
-        self.conv = nn.BatchNorm2d(num_features=num_features)
 
-    def forward(self, x):
+    def forward(self, input):
         tic = time.time()
-        x = self.conv(x)
+        output = F.batch_norm(input, self.running_mean, self.running_var, self.weight, self.bias,
+                              self.training, self.momentum, self.eps)
         toc = time.time()
-        self.times = toc-tic
-        return x
+        self.times = toc - tic
+        return output
 
 
-class AvgPoolWithTime(nn.Module):
-    def __init__(self, kernel_size, layer_name, stride=None, padding=0, ceil_mode=False,
+class AvgPool2dWithTime(nn.AvgPool2d):
+    def __init__(self, kernel_size, stride=None, padding=0, ceil_mode=False,
                  count_include_pad=True):
-        super(AvgPoolWithTime, self).__init__()
+        super(AvgPool2dWithTime, self).__init__(kernel_size, stride, padding, ceil_mode, count_include_pad)
         self.times = 0
-        self.layer_name = layer_name
-        self.conv = nn.AvgPool2d(kernel_size=kernel_size, stride=stride, padding=padding,
-                                 ceil_mode=ceil_mode,
-                                 count_include_pad=count_include_pad)
 
-    def forward(self, x):
+    def forward(self, input):
         tic = time.time()
-        x = self.conv(x)
+        output = F.avg_pool2d(input, self.kernel_size, self.stride,
+                              self.padding, self.ceil_mode, self.count_include_pad)
         toc = time.time()
-        self.times = toc-tic
-        return x
+        self.times = toc - tic
+        return output
 
 
-class MaxPoolWithTime(nn.Module):
-    def __init__(self, kernel_size, layer_name, stride=None, padding=0, dilation=1,
+class MaxPool2dWithTime(nn.MaxPool2d):
+    def __init__(self, kernel_size, stride=None, padding=0, dilation=1,
                  return_indices=False, ceil_mode=False):
-        super(MaxPoolWithTime, self).__init__()
+        super(MaxPool2dWithTime, self).__init__(kernel_size=kernel_size, stride=stride, padding=padding,
+                                                dilation=dilation, return_indices=return_indices,
+                                                ceil_mode=ceil_mode)
         self.times = 0
-        self.layer_name = layer_name
-        self.conv = nn.MaxPool2d(kernel_size=kernel_size, stride=stride,
-                                 padding=padding, dilation=dilation,
-                                 return_indices=return_indices, ceil_mode=ceil_mode)
 
-    def forward(self, x):
+    def forward(self, input):
         tic = time.time()
-        x = self.conv(x)
+        output = F.max_pool2d(input, self.kernel_size, self.stride,
+                              self.padding, self.dilation, self.ceil_mode,
+                              self.return_indices)
         toc = time.time()
-        self.times = toc-tic
-        return x
+        self.times = toc - tic
+        return output
 
 
-class MultiConv(nn.Module):
-    def __init__(self):
-        super(MultiConv, self).__init__()
-        self.times = {}
-        layer_classes = ['bn', 'conv']
-        self.conv = nn.Sequential(
-            ConvWithTime(3, 40, 3, 'conv'),
-            BNWithTime(40, 'bn'),
-            ConvWithTime(40, 40, 3, 'conv'),
-            BNWithTime(40, 'bn'),
-            ConvWithTime(40, 40, 3, 'conv'),
-            BNWithTime(40, 'bn'),
-            ConvWithTime(40, 40, 3, 'conv')
-        )
-        for i in layer_classes:
-            self.times[i] = 0
+class ReLUWithTime(nn.ReLU):
+    def __init__(self, inplace=False):
+        super(ReLUWithTime, self).__init__(inplace=inplace)
+        self.times = 0
 
-    def forward(self, x):
-        x = self.conv(x)
-        for i in model.children():
-            if isinstance(i, nn.Sequential):
-                for j in i:
-                    self.times[j.layer_name] += j.times
-        return x
+    def forward(self, input):
+        tic = time.time()
+        output = F.relu(input, self.inplace)
+        toc = time.time()
+        self.times = toc - tic
+        return output
+
+
+class LinearWithTime(nn.Linear):
+    def __init__(self, in_features, out_features, bias=True):
+        super(LinearWithTime, self).__init__(in_features=in_features,
+                                             out_features=out_features,
+                                             bias=bias)
+        self.times = 0
+
+    def forward(self, input):
+        tic = time.time()
+        output = F.linear(input, self.weight, self.bias)
+        toc = time.time()
+        self.times = toc - tic
+        return output
 
 
 if __name__ == '__main__':
     a = torch.randn(1, 3, 224, 224)
     a = Variable(a)
-    model = MultiConv()
+    model = Conv2dWithTime(3, 4, kernel_size=3, padding=1)
     b = model(a)
-    print(model.times['conv'], model.times['bn'])
+    print(model.weight)
+    print(model.times)
 
